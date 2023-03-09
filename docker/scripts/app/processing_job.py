fb54a1676a37e828077315c96c1611e9b6bf901b
kramer <kramerscs@gmail.com>
2023-03-08 22:23:45 -0300

    Adding notebook

--- docker/scripts/app/processing_job.py
+++ docker/scripts/app/processing_job.py
@@ -19,23 +19,21 @@ import re
 def read_csv(spark: SparkSession, bucket: str, file_name: str) -> DataFrame:
     """Read CSV files from S3 bucket"""
     print("Reading CSV for file", file_name)
-    
+
     df = (
         spark
         .read
         .format("csv")
         .options(header='true', inferSchema='true', delimiter=';')
-        .load(f"s3a://{bucket}/enade2017/*.txt")
-        .withColumn("file_name", f.input_file_name())
+        .load(f"s3a://{bucket}/enade2017/{file_name}")
     )
     return df
 
 
-
-def write_parquet(bucket: str, df: DataFrame, file_name: str) -> DataFrame:
+def write_parquet(bucket: str, df: DataFrame, directory_name: str) -> DataFrame:
     """Write PARQUET files to S3 bucket"""
-    print("Saving parquet for file", file_name)
-    df_path = f"s3a://{bucket}/enade2017"
+    print("Saving parquet for file", directory_name)
+    df_path = f"s3a://{bucket}/enade2017/{directory_name}"
     print(f"Path: {df_path}")
     (
         df
@@ -47,18 +45,20 @@ def write_parquet(bucket: str, df: DataFrame, file_name: str) -> DataFrame:
     return True
 
 
+def parse_directory_name(fl_name: str) -> str:
+    return fl_name.split("microdados2017_")[1].split(".txt")[0]
+
+
 if __name__ == "__main__":
     """Main ETL script definition."""
     spark = get_spark_session()
 
-    print("Processing data from landing to processing")
-    print("Reading Dataframe!")
-    df = read_csv(spark, LANDING_BUCKET, "ddd")
-    print("Done!")
-    print(f"Total lines:{df.count()}")
-    print(f"Dataframe original schema: {df.printSchema()}")
-    print(f"Schema after processing: {df.printSchema()}")
-    print("Writing Dataframe!")
-    write_parquet(PROCESSING_BUCKET, df, "ssss")
-    print("Done!")
+    print("Processing data from landing to processing now. ")
+
+    for fl_name in parse_file_names(ENADE_FOLDER):
+        df = read_csv(spark, LANDING_BUCKET, "ddd")
+        print("Done ----->", fl_name)
+        print("Writing Dataframe!")
+        write_parquet(PROCESSING_BUCKET, df, parse_directory_name(fl_name))
+        print("Done!")
     spark.stop()
